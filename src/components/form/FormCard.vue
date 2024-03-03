84f4d69cd377e2d8fc23fcef6582d549ed4abf60
Morgana Duarte <morganaianez@gmail.com>
2024-03-03 10:54:45 -0300

    adapt form to mobile

--- src/components/form/FormCard.vue
+++ src/components/form/FormCard.vue
@@ -1,32 +1,28 @@
 <template>
-  <div class="container">
-    <form>
+  <form>
+    <label class="paragraph-form">Nome*</label>
+    <input type="text" v-model="formData.name" placeholder="Nome e Sobrenome" class="input-form"/>
+    <div class="form-inner">
       <div>
-        <p class="paragraph-form">Nome*</p>
-        <input type="text" v-model="formData.name" placeholder="Nome e Sobrenome" class="input-form"/>
-      </div>
-      <div class="form-inner">
-        <div>
-          <p class="paragraph-form">Telefone*</p>
-          <input type="tel" v-model="formData.phone" placeholder="(00) 0 0000-0000" class="input-form" />
-        </div>
-        <div>
-          <p class="paragraph-form">E-mail*</p>
-          <input type="email" v-model="formData.email" placeholder="nome@email.com" class="input-form" />
-        </div>
+        <label class="paragraph-form">Telefone*</label>
+        <input type="tel" v-model="formData.phone" placeholder="(00) 0 0000-0000" class="input-form" />
       </div>
       <div>
-        <ul class="list-style">
-          <li class="list-form">Ao preencher o formulário, concordo * em receber comunicações de acordo com meus interesses.</li>
-          <li class="list-form">Ao informar meus dados, eu concordo com a Política de privacidade.</li>
-        </ul>
-        <p class="list-form">* Você pode alterar suas permissões de comunicação a qualquer tempo.</p>
+        <label class="paragraph-form">E-mail*</label>
+        <input type="email" v-model="formData.email" placeholder="nome@email.com" class="input-form" />
       </div>
-      <div>
-        <button @click="submitForm" class="button-generate">GERAR CARTÃO GRÁTIS</button>
-      </div>
-    </form>
-  </div>
+    </div>
+    <div>
+      <ul class="list-style">
+        <li class="list-form">Ao preencher o formulário, concordo * em receber comunicações de acordo com meus interesses.</li>
+        <li class="list-form">Ao informar meus dados, eu concordo com a Política de privacidade.</li>
+      </ul>
+      <p class="list-form">* Você pode alterar suas permissões de comunicação a qualquer tempo.</p>
+    </div>
+    <div>
+      <button @click="submitForm" class="button-generate">GERAR CARTÃO GRÁTIS</button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -50,12 +46,6 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  display: inline-flex;
-  width: 620px;
-  height: 336px;
-}
-
 .paragraph-form {
   color: #FFFFFF;
   font-family: "Nunito Sans", sans-serif;
@@ -70,11 +60,7 @@ export default {
 
 .form-inner {
   display: flex;
-  justify-content: space-between;
-}
-
-.list-style {
-  padding-left: 25px;
+  flex-wrap: wrap;
 }
 
 .list-form {
